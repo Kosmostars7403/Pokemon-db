d43ab6c20c3a0fd2dbe95ecc19b73af89777d019
Kosmostars7403 <3699818@mail.ru>
2020-07-09 20:33:27 +0300

    Update views.py

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,12 +1,9 @@
 import folium
 import os
 
-
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from pokemon_entities.models import Pokemon, PokemonEntity
-
-
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
@@ -30,13 +27,11 @@ def show_all_pokemons(request):
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemon_entities:
-
         full_image_path = request.build_absolute_uri(pokemon_entity.pokemon.image.url)
 
         add_pokemon(
             folium_map, pokemon_entity.lat, pokemon_entity.lon,
             pokemon_entity.pokemon.title, full_image_path)
-
 
     pokemons_on_page = []
     for pokemon in pokemons:
@@ -45,7 +40,6 @@ def show_all_pokemons(request):
             'img_url': pokemon.image.url,
             'title_ru': pokemon.title,
         })
-
 
     return render(request, "mainpage.html", context={
         'map': folium_map._repr_html_(),
@@ -82,7 +76,6 @@ def show_pokemon(request, pokemon_id):
             "img_url": pokemon_next_evolution.image.url
         }
 
-
     pokemon_entities = requested_pokemon.pokemon_entities.all()
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
@@ -94,5 +87,3 @@ def show_pokemon(request, pokemon_id):
 
     return render(request, "pokemon.html", context={'map': folium_map._repr_html_(),
                                                     'pokemon': pokemon_specification})
-
-
